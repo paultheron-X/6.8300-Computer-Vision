d149ef3ea5690ed3006122379fa3bff9dcef50a4
Paul Theron <72143205+PaulRaid@users.noreply.github.com>
2023-03-14 19:02:43 -0400

    format

--- src/data_handlers/loading/_dataloader.py
+++ src/data_handlers/loading/_dataloader.py
@@ -2,7 +2,9 @@ import os
 import sys
 import logging
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))) #for testing this file with main and so that utils can be imported
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+)  # for testing this file with main and so that utils can be imported
 
 import torch
 import torchvision.transforms as TF
@@ -15,7 +17,7 @@ from utils.arguments_parser import data_loading_parser
 
 
 class VideoDataset(Dataset):
-    def __init__(self, data_dir, img_size, rolling_window):
+    def __init__(self, data_dir, img_size=(512, 512), rolling_window=5):
         self.data_dir = data_dir
         self.img_size = img_size
         self.rolling_window = rolling_window
@@ -39,7 +41,7 @@ class VideoDataset(Dataset):
         for i in range(index, index + self.rolling_window):
             img_path = self.files[i]
             with Image.open(img_path) as img:
-                if i - index == self.rolling_window//2:
+                if i - index == self.rolling_window // 2:
                     # we save the target image before downsampling
                     target = TF.ToTensor()(img)
                 img = self.downsample(img)
@@ -51,19 +53,24 @@ class VideoDataset(Dataset):
 if __name__ == "__main__":
     args = data_loading_parser()
     logger_setup(args)
-    
-    
+
     dataset = VideoDataset(
         data_dir="data/processed", img_size=(512, 512), rolling_window=5
     )
-    
-    logging.info(f"Dataset length: {len(dataset)}")
-    logging.info(f"Dataset sample input shape: {dataset[0][0].shape}, should be (rolling_window, n_channels, img_size[0] // 4, img_size[1] // 4)")
-    logging.info(f"Dataset sample target shape: {dataset[0][1].shape}, should be (n_channels, img_size[0], img_size[1])")
 
+    logging.info(f"Dataset length: {len(dataset)}")
+    logging.info(
+        f"Dataset sample input shape: {dataset[0][0].shape}, should be (rolling_window, n_channels, img_size[0] // 4, img_size[1] // 4)"
+    )
+    logging.info(
+        f"Dataset sample target shape: {dataset[0][1].shape}, should be (n_channels, img_size[0], img_size[1])"
+    )
 
     dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
-    
-    logging.info(f"Dataloader sample input shape: {next(iter(dataloader))[0].shape}, should be (batch_size, rolling_window, n_channels, img_size[0] // 4, img_size[1] // 4)")
-    logging.info(f"Dataloader sample target shape: {next(iter(dataloader))[1].shape}, should be (batch_size, n_channels, img_size[0], img_size[1])")
-    
+
+    logging.info(
+        f"Dataloader sample input shape: {next(iter(dataloader))[0].shape}, should be (batch_size, rolling_window, n_channels, img_size[0] // 4, img_size[1] // 4)"
+    )
+    logging.info(
+        f"Dataloader sample target shape: {next(iter(dataloader))[1].shape}, should be (batch_size, n_channels, img_size[0], img_size[1])"
+    )
