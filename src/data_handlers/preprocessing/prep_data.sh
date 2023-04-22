9b17b71834534c1d7d09415023eb4aeac777c098
Paul Theron <paul.theron@aerides.polytechnique.fr>
2023-04-22 18:28:07 +0200

    scripts

--- src/data_handlers/preprocessing/prep_data.sh
+++ src/data_handlers/preprocessing/prep_data.sh
@@ -4,7 +4,8 @@ echo "Downloading REDS data from website https://seungjunnah.github.io/Datasets/
 mkdir -p /Data/reds_dataset/raw/train_orig
 
 
-for i in {0..15}; do
+for i in 0 1 4 7 9
+do
     echo "Prep part $i"
     scp /Data/reds_dataset/raw/train_orig_part$i.zip paul.theron@bentley.polytechnique.fr:/Data/reds_dataset/raw/
     #wget -c http://data.cv.snu.ac.kr:8008/webdav/dataset/REDS/orig/train_orig_part$i.zip -P /Data/reds_dataset/raw
