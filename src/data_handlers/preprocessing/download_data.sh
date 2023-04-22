c4eeea38cfa6330f118931afdd619ff5b0618013
Paul Theron <paul.theron@aerides.polytechnique.fr>
2023-04-22 18:55:43 +0200

    .

--- src/data_handlers/preprocessing/download_data.sh
+++ src/data_handlers/preprocessing/download_data.sh
@@ -9,7 +9,7 @@ for i in {0..15}; do
     echo "Downloading part $i"
     #wget -c http://data.cv.snu.ac.kr:8008/webdav/dataset/REDS/orig/train_orig_part$i.zip -P /Data/reds_dataset/raw
 
-    unzip /Data/reds_dataset/raw/train_orig_part$i.zip -d /Data/reds_dataset/raw
+    #unzip /Data/reds_dataset/raw/train_orig_part$i.zip -d /Data/reds_dataset/raw
 
-    cp /Data/reds_dataset/raw/train_orig_part$i/train_orig/* /Data/reds_dataset/raw/train_orig
+    cp /Data/reds_dataset/raw/train/train_orig/* /Data/reds_dataset/raw/train_orig
 done
