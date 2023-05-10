9a8d6a3c12f4a9b7fef0eb91e898f0dec4486575
Paul Theron <paul.theron@bentley.polytechnique.fr>
2023-05-10 04:47:28 +0200

    unzip

--- data/unzip_data.sh
+++ data/unzip_data.sh
@@ -1,5 +1,9 @@
 # process data:
 
+ln -s /Data/reds_dataset/raw/ data/raw
+
+ln -s /Data/reds_dataset/processed/ data/processed
+
 unzip /Data/reds_dataset/raw/val_sharp.zip -d /Data/reds_dataset/processed/val_sharp
 
 unzip /Data/reds_dataset/raw/val_sharp_bicubic.zip -d /Data/reds_dataset/processed/val_sharp_bicubic
